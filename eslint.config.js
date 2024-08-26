913fcea54abda873e9de2c8f7657ccb7e2ed579e
Aniket Kotal <mightykillrr@proton.me>
2024-08-26 12:11:42 +0530

    refactor: removed e2e/cypress testing and extra locales

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,52 @@
 // @ts-check
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
 
-export default antfu(
-  {
-    unocss: true,
-    formatters: true,
+export default antfu({
+  formatters: true,
+  vue: {
+    sfcBlocks: { blocks: { customBlocks: ["route"] } },
+    overrides: {
+      "vue/component-name-in-template-casing": [
+        "error",
+        "PascalCase",
+        { registeredComponentsOnly: false },
+      ],
+      "vue/block-order": [
+        "error",
+        { order: ["script", "template", "style"] },
+      ],
+    },
   },
-)
+  stylistic: {
+    quotes: "double",
+    indent: 2,
+    semi: true,
+  },
+  ignores: ["src/styles", "src/assets"],
+}, {
+  rules: {
+    "style/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+    "unused-imports/no-unused-vars": "off",
+    "no-console": "off",
+    "curly": ["error", "multi-line"],
+    "jsdoc/require-returns-description": "off",
+    "object-curly-newline": [
+      "error",
+      {
+        ObjectExpression: {
+          multiline: true,
+          minProperties: 2,
+        },
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: {
+          multiline: true,
+          minProperties: 2,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 2,
+        },
+      },
+    ],
+  },
+});
--- uno.config.ts
+++ uno.config.ts
@@ -7,25 +7,20 @@ import {
   presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
-} from 'unocss'
+} from "unocss";
 
 export default defineConfig({
-  shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
-  ],
+  shortcuts: [],
   presets: [
     presetUno(),
     presetAttributify(),
-    presetIcons({
-      scale: 1.2,
-    }),
+    presetIcons({ scale: 1.2 }),
     presetTypography(),
     presetWebFonts({
       fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
+        sans: "DM Sans",
+        serif: "DM Serif Display",
+        mono: "DM Mono",
       },
     }),
   ],
@@ -33,5 +28,5 @@ export default defineConfig({
     transformerDirectives(),
     transformerVariantGroup(),
   ],
-  safelist: 'prose prose-sm m-auto text-left'.split(' '),
-})
+  // safelist: 'prose prose-sm m-auto text-left'.split(' '),
+});
