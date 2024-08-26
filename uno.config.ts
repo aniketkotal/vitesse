42454e5f5ef1886847a8c078ee4554d7d2c327d3
Aniket Kotal <mightykillrr@proton.me>
2024-08-26 12:27:45 +0530

    reverted uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,10 @@ import {
 } from "unocss";
 
 export default defineConfig({
-  shortcuts: [],
+  shortcuts: [
+    ["btn", "px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50"],
+    ["icon-btn", "inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600"],
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
@@ -28,5 +31,5 @@ export default defineConfig({
     transformerDirectives(),
     transformerVariantGroup(),
   ],
-  // safelist: 'prose prose-sm m-auto text-left'.split(' '),
+  safelist: "prose prose-sm m-auto text-left".split(" "),
 });
